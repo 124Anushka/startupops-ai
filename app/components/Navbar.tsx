85b0e0d9eb0921f2bce1cf231db5833db97c8330
Anushka Singh <165248103+124Anushka@users.noreply.github.com>
2026-02-21 22:44:46 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,18 @@
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <div className="nav-inner">
+    <nav className="navbar">
+      <div className="logo"> StartupOps AI</div>
 
-        <a href="/" className="nav-logo">
-          StartupOps AI
-        </a>
-
-        <div className="nav-center">
+        <div className="nav-links">
           <a href="#how">How it works</a>
           <a href="#who">Who it’s for</a>
           <a href="#pricing">Pricing</a>
           <a href="#contact">Contact</a>
         </div>
 
-        <a href="#pricing" className="nav-cta">
+        <a href="https://chatgpt.com/g/g-69620ff898308191a9da31cfffe153c3-startupops-ai" className="btn-primary"
+          target="_blank"
+          >
           Start Free
         </a>
 
