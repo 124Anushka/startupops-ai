13979108cda136bb71fa0260612e1ccefbe56626
Anushka Singh <165248103+124Anushka@users.noreply.github.com>
2026-02-23 21:26:23 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
