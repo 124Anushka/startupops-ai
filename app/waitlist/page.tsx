ae469ff6a6332c67b386965f0a30f3b2000e9cac
Anushka Singh <165248103+124Anushka@users.noreply.github.com>
2026-02-25 22:40:54 +0530

    Create page.tsx

---
+++ app/waitlist/page.tsx
@@ -0,0 +1,24 @@
+export default function WaitlistPage() {
+  return (
+    <main className="section">
+      <div className="container center">
+
+        <h1>Join Early Access</h1>
+
+        <p className="subtitle">
+          Get priority access, early pricing, and direct collaboration opportunities.
+        </p>
+
+        <div style={{ marginTop: "40px" }}>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSemCbzt7rmajlYoIOtmVcLHTPgUUxJ0Dl8K8JPSc45Fis9GCQ/viewform?embedded=true"
+            width="100%"
+            height="800"
+            style={{ border: "none" }}
+          ></iframe>
+        </div>
+
+      </div>
+    </main>
+  );
+}
