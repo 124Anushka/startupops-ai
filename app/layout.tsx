454a873555c7001a9d8a2d5cc2e6ec31a0eafbbd
Anushka Singh <165248103+124Anushka@users.noreply.github.com>
2026-02-26 20:11:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Script from "next/script"
 
 export default function RootLayout({
   children,
