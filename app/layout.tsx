cc1e4150386b8c4145115250572c6f562c6a2715
Anushka Singh <165248103+124Anushka@users.noreply.github.com>
2026-02-26 20:08:54 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,30 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
+  <html lang="en">
+    
+    <head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5BM0MN4C5P"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-5BM0MN4C5P');
+        `}
+      </Script>
+    </head>
+
+    <body>
+      <Navbar />
+      {children}
+      <Footer />
+    </body>
+
+  </html>
+)
 }
